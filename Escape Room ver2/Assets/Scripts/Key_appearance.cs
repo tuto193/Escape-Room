cc00d5de64177b9d9df2d2ea7ac743193f2a0c2e
Julian Webb <jwebb@uni-osnabrueck.de>
2021-04-11 12:02:49 +0200

    Schlüssel hinzugefügt

---
+++ Escape Room ver2/Assets/Scripts/Key_appearance.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key_appearance : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hide the key on start
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Show the key if all riddles have been completed 
+        // OR
+        // notify the key to appear when all riddles have been completed
+    }
+}
