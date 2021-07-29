489fc786ccdf3a0bb334554cb3a6df24cfd3193c
Carlos A. Parra F <carlos.a.parra.f@gmail.com>
2021-07-29 17:57:14 +0200

    Carry on with ECS stuff on big PC

--- Assets/Scripts/WeaponSystem/FiringConfiguration/WeaponLocation.cs
+++ Assets/Scripts/WeaponSystem/FiringConfiguration/WeaponLocation.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 
 public class WeaponLocation : MonoBehaviour {
     void OnDrawGizmos() {
-        DrawArrow.ForGizmo(Vector3(0, 0, 0), Vector3(0, 100, 0), 4);
-        DrawArrow.ForGizmo(Vector3(-15, -85, 0), Vector3(15, -115, 0), 4);
-        DrawArrow.ForGizmo(Vector3(-15, -115, 0), Vector3(15, -85, 0), 4);
+        DrawArrow.ForGizmo(new Vector3(0, 0, 0), new Vector3(0, 100, 0), 4);
+        DrawArrow.ForGizmo(new Vector3(-15, -85, 0), new Vector3(15, -115, 0), 4);
+        DrawArrow.ForGizmo(new Vector3(-15, -115, 0), new Vector3(15, -85, 0), 4);
     }
 }
--- Assets/Scripts/WeaponSystem/Projectile/Projectile.cs
+++ Assets/Scripts/WeaponSystem/Projectile/Projectile.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
-    public delegate void CollidedEvent(GameObject target, Vector3 hit_location);
+    // Signal when hitting a propper target
+    public delegate void collidedEventHandler(GameObject target, Vector3 hit_location);
+    public event collidedEventHandler collidedEvent;
+    // If not hitting a target
+    public delegate void missedEventHandler(Vector3 miss_location);
+    public event missedEventHandler missedEvent;
+
+    [Tooltip("How long the projectile will travel for")]
+    private float lifetime = 1.0f;
+    // [Tooltip("What kind of trajectory/motions the projectile has")]
+    // private
+
 
 }
